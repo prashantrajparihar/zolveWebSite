2bf521ab04aff2449c5a2819f1f8e376b5b01a0e
prashantrajparihar <prashantrajparihar@gmail.com>
2021-03-05 15:48:55 +0530

    zolve 2.2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import RootReducers from "./rootReducers";
 
-const store = createStore(RootReducers, applyMiddleware(thunk, logger));
+const store = createStore(RootReducers, applyMiddleware(thunk));
 export default store;
